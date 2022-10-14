7f642b868995eb7a6400de6adf92e50f09d939fb
Caden Perez <przcaden@gmail.com>
2022-10-14 15:00:49 -0500

    write selection functions

--- genetic-algorithm.py
+++ genetic-algorithm.py
@@ -8,58 +8,20 @@
 ############################################################################################
 
 
+from email.mime import base
 import igraph as ig
 import matplotlib.pyplot as plt
 import random
 
-NUM_RANDOM_GENERATIONS = 8
+# Define algorithm constants
+NUM_RANDOM_EDGES = 8
+MUTATION_RATE = 0.01
 NUM_GENERATIONS = 5
 NUM_NODES = 19
 NUM_EDGES = 37
 
-#           Genetic algorithm pseudocode
-# begin 
-#     generation = 0 
-#     while ( best_fitness != 0 ): 
-#         selection(population) 
-#         crossover(population) 
-#         mutation(population) 
-#         if ( Best(population) < best_fitness ): 
-#             then best_fitness = Best(population) 
-#         generation += 1 
-#     end while 
-#     return best_fitness 
-# end 
-
-
-
-
-#           Selection function pseudocode
-#       Inputs: 
-#       Returns: 
-# begin
-#     for each f in fitnesses:
-#         for each parent of node:
-#             select 2 parents with highest fitnesses?
-
-
-
-
-#           Fitness-determining function pseudocode
-# every time one of the target nodes appears in a state, increment fitness by 1
-#       Inputs: list of traversed edges (a state)
-#       Returns: an integer (rating of fitness).
-# begin
-#     fitness = 0
-#     for each edge=True in state:
-#         if edge is connected to a highlighted node:
-#             fitness += 1
-#         if edge connects two highlighted nodes:
-#             fitness += 2
-#     return fitness
-
 # Function that determines the given fitness of a given generation.
-# Pre: state has been initialized with traversed edges.
+# Pre: generation has been initialized with traversed edges.
 #      all node connection data has been created and user has selected nodes.
 # Post: fitness of the given state has been calculated.
 def determineStateFitness(state, n_data, h_nodes):
@@ -71,6 +33,9 @@ def determineStateFitness(state, n_data, h_nodes):
             fitness += edgeFitness(n_data[i], h_nodes)
     return fitness
 
+# Determines the fitness of a specific edge.
+# Pre: generation has been initialized with traversed edges and user has selected nodes.
+# Post: fitness of the given edge has been calculated.
 def edgeFitness(connection, h_nodes):
     fitness = 0
     # Check if edge connects highlighted node(s).
@@ -82,17 +47,42 @@ def edgeFitness(connection, h_nodes):
     return fitness
 
 # Randomly generate a set of edges, which will be the initial population.
-def random_population(path_traversed):
-    new_path = [path_traversed]
+def random_population():
+    new_path = [False] * NUM_EDGES
     generated_edges = []
-    for i in range(NUM_RANDOM_GENERATIONS):
+    for i in range(NUM_RANDOM_EDGES):
         rand_edge = random.randint(0,NUM_EDGES-1) # random index
         while rand_edge in generated_edges:
             rand_edge = random.randint(0,NUM_EDGES-1)
         generated_edges.append(rand_edge)
-        print(rand_edge)
         new_path[rand_edge] = True
     return new_path
+
+# Generate a probability for each edge in a population to be selected
+def get_probabilities(population, n_data, h_nodes):
+    fitnesses = []
+    for i in range(NUM_EDGES):
+        if population[i]:
+            fitnesses.append(edgeFitness(n_data[i], h_nodes))
+    total_fitness = sum(fitnesses)
+    relative_fitnesses = [f/total_fitness for f in fitnesses]
+    print('Fitnesses: ', fitnesses)
+    print('Relative fitnesses: ', relative_fitnesses)
+    probabilities = [sum(relative_fitnesses[:i+1]) for i in range(len(relative_fitnesses))]
+    print('Edge probabilities: ', probabilities)
+    return probabilities
+
+# Select two parents for crossover based on generated probabilities
+def selection(population, probabilities):
+    chosen_edges = []
+    for i in range(2):
+        r = random.random()
+        for (i, edge) in enumerate(population):
+            print('i: ',i,' edge: ', edge)
+            if r <= probabilities[i]:
+                chosen_edges.append(edge)
+                break
+    return chosen_edges
 
 def main():
     # Get user inputted values for connected nodes:
@@ -110,11 +100,10 @@ def main():
     # Initialize graph data
     node_names = []
     highlighted_nodes = []
-    path_traversed = [] # highlighted edges (bool)
+    base_population = [False]*NUM_EDGES # highlighted edges (bool)
     network_data = [[]] # data of all
 
-    print(NUM_NODES)
-    print(connecting_nodes)
+    # Create node properties
     for i in range(NUM_NODES):
         # Append a value for the node
         node_names.append(str(i))
@@ -122,11 +111,6 @@ def main():
         if str(i) in connecting_nodes:
             highlighted_nodes.append(True)
         else: highlighted_nodes.append(False)
-    print(highlighted_nodes)
-
-    for i in range(NUM_EDGES):
-        # Initialize path highlighting (False = not traversed yet)
-        path_traversed.append(False)
 
     # Get graph path data
     file = open("node_data.txt", "r")
@@ -135,7 +119,7 @@ def main():
             network_data.append( (int(l.split()[0]), int(l.split()[1]))  )
     network_data.remove([])
 
-    # Set graph characteristics
+    # Set initial graph characteristics
     g = ig.Graph(NUM_NODES, network_data)
     g["title"] = "Genetic Network"
     g.vs["names"] = node_names
@@ -143,56 +127,37 @@ def main():
     g.es["connections"] = network_data
     g.es["edges"] = [False] * NUM_EDGES
 
-    # Plot graph in matplotlib
-    fig, ax = plt.subplots(figsize=(5,5))
-    ig.plot(
-        g,
-        target = ax,
-        vertex_size=0.3,
-        vertex_color=["steelblue" if node_highlighted else "salmon" for node_highlighted in g.vs["nodes"]],
-        vertex_frame_width=2.0,
-        vertex_frame_color="white",
-        vertex_label=g.vs["names"],
-        vertex_label_size=7.0,
-        edge_width=[2 if edge_traversed else 1 for edge_traversed in g.es["edges"]],
-        edge_color=["#7142cf" if edge_traversed else "#AAA" for edge_traversed in g.es["edges"]]
-    )
-    plt.show()
-
     # Generate initial population
-    g.es["edges"] = random_population(path_traversed)
-    plt.draw()
+    g.es["edges"] = random_population()
 
-    populations = [[]]
-    fitnesses = []
-    newPopulation = []
-    populations.append(random_population())
-    determineStateFitness(populations[0])
+    # Begin genetic algorithm
+    populations = [base_population]
+    for current_generation_index in range(NUM_GENERATIONS):
+        # Plot graph in matplotlib
+        fig, ax = plt.subplots(figsize=(5,5))
+        ig.plot(
+            g,
+            target = ax,
+            vertex_size=0.25,
+            vertex_color=["steelblue" if node_highlighted else "salmon" for node_highlighted in g.vs["nodes"]],
+            vertex_frame_width=2.0,
+            vertex_frame_color="white",
+            vertex_label=g.vs["names"],
+            vertex_label_size=9.0,
+            edge_width=[2 if edge_traversed else 1 for edge_traversed in g.es["edges"]],
+            edge_color=["#7142cf" if edge_traversed else "#AAA" for edge_traversed in g.es["edges"]]
+        )
+        plt.show()
 
-    # while i in range(NUM_GENERATIONS):
-        # update_graph()
-        # selection():
-        #   - compare fitness of all edges: one that are more fit get selected
-        #   - if more than one state in states, compare completeness of all states
-        #   - goal is to find another generation
+        # Perform selection
+        population_probabilities = get_probabilities(g.es["edges"], network_data, highlighted_nodes)
+        parents = selection(g.es["edges"], population_probabilities)
+        print(parents)
 
-        # def propagate(self):
-        #     newPopulation = []
-        #     while len(newPopulation) < len(self.population):
-        #         parents = self.chooseParents()
-        #         if random() < self.crossover_chance:
-        #             [child1, child2] = parents[0].crossover(parents[1])
-        #             newPopulation.append(child1)
-        #             newPopulation.append(child2)
-        #         else:
-        #             newPopulation.append(parents[0])
-        #             newPopulation.append(parents[1])
-        #     if len(newPopulation) > len(self.population):
-        #         newPopulation.pop()
-        #     for potentialSolution in newPopulation:
-        #         if random() < self.mutation_chance:
-        #             potentialSolution.mutate()
-        #     self.population = newPopulation
+        # Step up to next generation (temporary)
+        user_input = ''
+        while user_input == '':
+            user_input = input('Enter any value to proceed to next generation: ')
 
 if __name__ == "__main__":
     main()
